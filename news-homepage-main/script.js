a1549a83548551aa035ccd8aff55f6e9dc924baf
M-Ahmad-ma <153421053+M-Ahmad-ma@users.noreply.github.com>
2024-01-30 13:54:43 +0000

    Add files via upload

---
+++ news-homepage-main/script.js
@@ -0,0 +1,8 @@
+let closeBtn = document.getElementById("close-btn");
+let openBtn = document.getElementById("open-btn");
+let list = document.getElementById("list")
+
+openBtn.addEventListener('click', function(){
+    list.style.left = '300'
+
+})
