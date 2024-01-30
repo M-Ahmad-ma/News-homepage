b330485efa94025cbb3b101212b593c9ed45f283
M-Ahmad-ma <153421053+M-Ahmad-ma@users.noreply.github.com>
2024-01-30 17:37:14 +0000

    Update script.js

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,8 +1,11 @@
 let closeBtn = document.getElementById("close-btn");
 let openBtn = document.getElementById("open-btn");
-let list = document.getElementById("list")
+let list = document.getElementById("list");
 
 openBtn.addEventListener('click', function(){
     list.style.left = '300'
 
-})
+});
+closeBtn.addEventListener('click', function(){
+    list.style.left = '-800px'
+});
